36e4f15f7d6982ee28f60e4b4cf014322a91cb68
Helios77760 <helios77@outlook.fr>
2018-03-10 14:48:12 +0100

    Corpus Analyzer fonctionnel pour 1 fichier

--- src/dylanbrasseur/Main.java
+++ src/dylanbrasseur/Main.java
@@ -1,10 +1,10 @@
 package dylanbrasseur;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Vector;
+import java.util.*;
+
 
 class Mots
 {
@@ -19,6 +19,12 @@ class Mots
     @Override
     public boolean equals(Object obj) {
         return obj instanceof Mots && ((Mots) obj).value.equals(value);
+    }
+
+    @Override
+    public String toString()
+    {
+        return value;
     }
 }
 
@@ -37,14 +43,43 @@ class Entry
     public boolean equals(Object obj) {
         return obj instanceof Entry && ((Entry) obj).value.equals(value);
     }
+
+    @Override
+    public String toString() {
+        return value + "\t" + occurences + "\t" + followToString();
+    }
+
+    public String followToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Mots> arr = new ArrayList<>();
+        for(String key : follow.keySet())
+        {
+            arr.add(follow.get(key));
+        }
+        arr.sort((m,n)->{
+            if(m.occurences > n.occurences)
+                return -1;
+            else if(m.occurences == n.occurences)
+                return 0;
+            return 1;
+        });
+        for(int i=0; i<Main.maxPropositions && i<arr.size();i++)
+        {
+            sb.append(arr.get(i)).append("\t");
+        }
+        return sb.toString();
+    }
 }
 
 public class Main {
-    private static PrintThread print;
+    private static final Vector<String> printQueue = new Vector<>();
+    private static PrintEngine print;
     private static HashMap<String, Entry> wordAndFollow;
     private static boolean folderInputMode = false;
     private static String path="";
     private static final String dictionaryName="dictionary.txt";
+    public static final int maxPropositions = 4;
 
     public static void main(String[] args) {
         if(args.length < 1)
@@ -83,14 +118,16 @@ public class Main {
         }
 
         wordAndFollow = new HashMap<>();
-        print = new PrintThread();
+        print = new PrintEngine();
+        print.start();
+
 
         try(BufferedReader br = new BufferedReader(new InputStreamReader(Main.class.getClassLoader().getResourceAsStream(dictionaryName)))){
             String line;
             Integer frequencie;
             while((line = br.readLine() )!= null)
             {
-                String[] values = line.split(" ");
+                String[] values = line.split("\t");
                 try
                 {
                     frequencie = Integer.parseInt(values[1]);
@@ -104,7 +141,7 @@ public class Main {
         } catch(IOException e) {
             e.printStackTrace();
         }
-
+        wordAndFollow.put("", new Entry("", 0));
         if(folderInputMode)
         {
             try {
@@ -120,6 +157,46 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
+        addPrint("\nWriting results... ");
+        try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dictionnaryFinal.txt"))))
+        {
+            if(wordAndFollow.containsKey(""))
+            {
+                Entry entry = wordAndFollow.get("");
+                bw.write(entry.followToString());
+                bw.newLine();
+                wordAndFollow.remove("");
+            }
+
+            ArrayList<Entry> arr = new ArrayList<>();
+            for(String key : wordAndFollow.keySet())
+            {
+                arr.add(wordAndFollow.get(key));
+            }
+            arr.sort(Comparator.comparing(a -> a.value));
+            for (Entry anArr : arr) {
+                bw.write(anArr.toString());
+                bw.newLine();
+            }
+            addPrint("Done\n");
+
+        } catch (IOException e) {
+            addPrint("Failed\n");
+            e.printStackTrace();
+        }
+
+        try {
+            print.termination();
+            synchronized (printQueue)
+            {
+                printQueue.notifyAll();
+            }
+            print.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static String prepareLine(String line)
@@ -132,23 +209,31 @@ public class Main {
 
     public static void processFile(File file) throws IOException
     {
+
         if(file != null && file.exists() && file.getName().contains(".txt"))
         {
-            print.addString("Processing "+file.getName()+"... ");
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line;
+            addPrint("Processing "+file.getName()+"... ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+            String line, related;
             while((line = br.readLine())!= null)
             {
                 line = prepareLine(line);
                 String[] sentences = line.split("[.?!:;]");
                 for(String sentence : sentences)
                 {
-                    String[] words = sentence.split("[-, \"']");
-                    //TODO
+                    related="";
+                    String[] words = sentence.split("[-, \t\"'«»’–)(]");
+                    for(String word : words)
+                    {
+                        if(addWord(word, related))
+                        {
+                            related=word;
+                        }
+                    }
                 }
             }
+            addPrint("OK\n");
         }
-
     }
 
     public static void processFolder(String folderPath) throws IOException
@@ -164,10 +249,10 @@ public class Main {
                     try
                     {
                         processFile(file);
-                        print.addString("OK\n");
+
                     }catch(IOException e)
                     {
-                        print.addString("Failed\n");
+                        addPrint("Failed\n");
                     }
                 }
             }
@@ -175,25 +260,64 @@ public class Main {
             throw new FileNotFoundException("Folder doesn't exist");
     }
 
-    public static class PrintThread extends Thread {
-        private Vector<String> printQueue;
-        PrintThread()
+    public static boolean addWord(String word, String related)
+    {
+        if(word.length()>2 && wordAndFollow.containsKey(related) && wordAndFollow.containsKey(word)) //In the dictionnary
         {
-            printQueue = new Vector<>();
+            Entry entry = wordAndFollow.get(related);
+            if(entry.follow.containsKey(word))
+            {
+                entry.follow.get(word).occurences++;
+            }else
+            {
+                entry.follow.put(word, new Mots(word, 1));
+            }
+            return true;
         }
+        return false;
+    }
+    static void addPrint(String str)
+    {
+        synchronized (printQueue)
+        {
+            printQueue.add(str);
+            printQueue.notifyAll();
+        }
+
+    }
+
+    public static class PrintEngine extends Thread {
+
+        boolean terminate = false;
         @Override
         public void run() {
-            while(!printQueue.isEmpty())
-            {
-                System.out.print(printQueue.remove(0));
+            String str="";
+            boolean print=false;
+            while(!terminate || !printQueue.isEmpty()) {
+                synchronized (printQueue) {
+                    if(printQueue.isEmpty())
+                    {
+                        try {
+                            printQueue.wait();
+                        } catch (InterruptedException ignored) {
+
+                        }
+                    }
+                    if(!printQueue.isEmpty())
+                    {
+                        str = printQueue.remove(0);
+                        print=true;
+                    }
+
+                }
+                if(print)
+                    System.out.print(str);
             }
         }
 
-        void addString(String str)
+        public void termination()
         {
-            printQueue.add(str);
-            if(!this.isAlive())
-                this.start();
+            terminate=true;
         }
     }
 }
